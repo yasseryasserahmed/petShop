65c7a3c5f30cb8616cb96cb54302dbe9bd370ba1
yasseryasserahmed <belkaahmed098@gmail.com>
2026-08-03 13:41:15 +0200

    Initial commit with the project backend structure

---
+++ notification-service/src/test/java/com/petshop/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petshop.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
